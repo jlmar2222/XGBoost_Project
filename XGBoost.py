77b7462b16dc1334186ec6f71ee7f2916fb784eb
jlmar2222 <jlmar2222@gmail.com>
2025-12-16 17:06:05 +0100

    adding bulding tree, leaves nodes and predictions for single tree

--- XGBoost.py
+++ XGBoost.py
@@ -5,12 +5,15 @@ import numpy as np
 
 class Stump:
     def __init__(self, data,k):
-        super().__init__()
-        self.data = data 
+        super().__init__()       
+
+        self.data = data # data contiene y, X, g, h, w en ese orden        
         self.y = self.data[:,0:1]
         self.X = self.data[:,1:k]
         self.g = self.data[:,k:k+1]
         self.h = self.data[:,k+1:k+2]  
+
+        self.w = self.data[:,k+2:k+3]
 
         # Guardará aqui los datos una vez ejecutado el split
         self.feature = None
@@ -32,7 +35,7 @@ class Stump:
         D = np.concatenate((self.X, self.h), axis=1) # Espacio de instancias con pesos
         splits = {} # mejor split por feature
         
-        for k in range(D.shape[1]):
+        for k in range(D.shape[1]-1): # quitamos la dimension de h y recorremos solo los features
         #(1) Get Candidates
             candidates = [] # Serán los Upper Bounds de los bins representativos de los quantiles
 
@@ -54,7 +57,7 @@ class Stump:
                     candidates.append(x_i) # candidates ~ [min(x_i), max(x_i)]   
                     accumulated = 0             
         #(2) Save Scores (based on Gains => Equación (7) del paper original)
-            father = self.data[:,[k+1,-2,-1]] # coge Columna del feature, gradiante y hessiano
+            father = self.data[:,[k+1,-3,-2]] # coge Columna del feature, gradiante y hessiano
             G = np.sum(father[:,-2])
             H = np.sum(father[:,-1])
 
@@ -65,6 +68,8 @@ class Stump:
             for i in range(len(candidates)):
 
                 left_son = father[father[:, 0] < candidates[i]]
+
+
                 # Calculamos gradiante/hessiano acumulado del posible hijo (a la izquierda)
                 G_l = np.sum(left_son[:,-2])
                 H_l = np.sum(left_son[:,-1])
@@ -100,8 +105,9 @@ class Stump:
 
         # Create Stump instances based in this new splits
         # formamos Devolvemos 2 nuevos Stumps dentro del msimo Stump
-        L_son = Stump(L_data, k)
-        R_son = Stump(R_data,k)
+        # Ponemos un control para Evitar crear nodos vacíos (= None)
+        L_son = Stump(L_data, k) if L_data.shape[0] > 0 else None # k = self.X.shape[1]
+        R_son = Stump(R_data, k) if R_data.shape[0] > 0 else None
 
         # Guardamos resultados par ael objeto
         self.feature = feature
@@ -115,42 +121,81 @@ class Stump:
 
 class Tree():
 
-    def __init__(self, data, k,max_depth):
+    def __init__(self, data, k, max_depth):
         super().__init__()
         self.k = k
         self.data = data
         self.max_depth = max_depth 
-        
+
         self.root = None
+        self.leaves = None
         # revisar porque puede ser que arboles sean mas pequeños que maxdepth 
         # esto puede hacer que pete imagino 
-        
-    def build_tree(self, data, eps, alpha, depth=0):
-        node = Stump(data, self.k)
+        self.new_data = None
+    
+    def build_tree(self, data, eps, alpha, depth = 0, min_samples = 1):
 
-        if depth >= self.max_depth or len(data) <= 1:
+        node = Stump(data,self.k)
+
+        if depth >= self.max_depth or data.shape[0] < min_samples:        
             return node
 
-        Lnode, Rnode = node.do_split(eps, alpha)
-
-        node.left = self.build_tree(Lnode.data, eps, alpha, depth + 1)
-        node.right = self.build_tree(Rnode.data, eps, alpha, depth + 1)
+        # Solo continuamos construyendo si el nodo no es None
+        L_node, R_node = node.do_split(eps,alpha) 
+        if L_node is not None:
+            node.left = self.build_tree(L_node.data,eps,alpha, depth = depth + 1)
+        if R_node is not None:
+            node.right = self.build_tree(R_node.data, eps, alpha, depth = depth + 1)
 
         return node
 
-    def fit(self, data, eps, alpha):
+    def fit(self, data, eps, alpha): # desarrolla el arbol en su plenitud, desde la raiz
         self.root = self.build_tree(data, eps, alpha)
 
-            
-          
 
+    def get_leaves(self,node): # recorre el arbol y devuelve los nodos hoja
+        
+    # Caso base: si el nodo no tiene hijos, es hoja
+        if node.left is None and node.right is None:
+            return [node]  # devolvemos la data del nodo hoja
+        
+        leaves = []
+        if node.left:
+            leaves += self.get_leaves(node.left)
+        if node.right:
+            leaves += self.get_leaves(node.right)        
+        
+        self.leaves = leaves
+        return leaves      
+        
+        #return leafs = [leaf,leaf,...,leaf] # lista de nodos hoja (Stumps)
+
+
+    def do_predicttion(self,leaves,alpha): # prectition = weight of the leaf based on equation (5)
+        new_leafs = []
+        for leaf in leaves:
+
+            G_j = np.sum(leaf.data[:,-2])
+            H_j = np.sum(leaf.data[:,-1])
+
+            weight = - (G_j / (H_j + alpha))
+
+            leaf.data[:,-1] = weight # ponemos las nuevas predicciones en la columna de w
+            new_leafs.append(leaf.data)
+        
+        self.new_data = np.vstack(new_leafs)    
+
+           
+    
+                      
 
 ###   PRUEBA   ########################    
 
+np.random.seed(42)  # <- seed fija
 
 # Definimos n y k para el ejemplo
-n = 5  # Número de filas
-k = 3  # Número de columnas para X
+n = 20  # Número de filas
+k = 2  # Número de columnas para X
 
 # Creamos las matrices individuales con dimensiones aleatorias
 y = np.random.rand(n, 1) # (n, 1)
@@ -158,15 +203,24 @@ X = np.random.rand(n, k) # (n, k)
 g = np.random.rand(n, 1) # (n, 1)
 h = np.random.rand(n, 1) # (n, 1)
 
+w = np.random.rand(n, 1) # (n, 1) añadimo suna ultima fila para los pesos
+
 # Concatenamos las matrices horizontalmente para formar 'data'
-data = np.concatenate((y, X, g, h), axis=1)
+data = np.concatenate((y, X, g, h, w), axis=1)
 
 
-Nodo1 = Stump(data, k)
+model = Tree(data,3,3)
 
-L ,R = Nodo1.do_split(0.05,0.5)
+model.fit(data,0.05,0.05)
+model.get_leaves(model.root)
+
+model.do_predicttion(model.leaves,0.05)
+
+new_data = model.new_data
 
 
-print(L.data)
-print('\n')
-print(Nodo1.right.data)
+#print(model.leaves[1].data)
+#print(new_data)
+
+#print(new_data)
+print(data)
